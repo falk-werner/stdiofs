a600a616e61a8942f585c9f5e4b0de842ef9db13
Falk Werner <falk.werner@gmx.net>
2021-07-24 22:29:15 +0200

    chore(deps): updated dependencies

--- deps.sh
+++ deps.sh
@@ -1,13 +1,13 @@
 PACKAGES="fuse3 gtest"
 
-fuse3_VERSION=3.10.0
+fuse3_VERSION=3.10.4
 fuse3_URL=https://github.com/libfuse/libfuse/archive/fuse-${fuse3_VERSION}.tar.gz
-fuse3_MD5=22aec9bc9008eea6b17e203653d1b938
+fuse3_MD5=186ac0ce03e55c997b1014b3ae46a4c4
 fuse3_DIR=libfuse-fuse-${fuse3_VERSION}
 fuse3_TYPE=meson
 
-gtest_VERSION=1.10.0
+gtest_VERSION=1.11.0
 gtest_URL=https://github.com/google/googletest/archive/release-${gtest_VERSION}.tar.gz
-gtest_MD5=ecd1fa65e7de707cd5c00bdac56022cd
+gtest_MD5=e8a8df240b6938bb6384155d4c37d937
 gtest_DIR=googletest-release-${gtest_VERSION}
 gtest_TYPE=cmake
